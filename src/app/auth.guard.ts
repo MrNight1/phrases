b1d9f83140cfead0d87b1995ac3d8ea38bb24e38
CHARMIN-PC\Oscar David <o.d.anota@gmail.com>
2018-07-18 12:42:00 -0500

    whitespace

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     // return true;
-    if (this.authService.isLoggednIn()){
+    if (this.authService.isLoggednIn()) {
       return true;
     } else {
       this.ngZone.run(() => this.router.navigate(['/']));
